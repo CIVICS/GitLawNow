4954d16e0a8d07905630f458525119edc9453620
Christian Smith <smith@anvil.io>
2017-11-05 21:52:16 -0500

    filter results

--- Complaints.js
+++ Complaints.js
@@ -1,4 +1,8 @@
-// Let's fetch some data.
+// Let's fetch some data and filter out the results that
+// are not owned by "CFPB Administrator"
 fetch('http://data.consumerfinance.gov/api/views.json')
-  .then(data => data.json())
-  .then(console.log)
+.then(data => data.json())
+.then(data => {
+    let owners = data.filter(doc => doc.owner.displayName !== 'CFPB Administrator')
+    console.log(owners)
+})
