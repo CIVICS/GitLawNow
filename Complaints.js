1352d5bd3d0e5f0a2acaaf92723e39ec4e8fb2f2
Christian Smith <smith@anvil.io>
2017-11-05 21:54:55 -0500

    get a list of owner names

--- Complaints.js
+++ Complaints.js
@@ -1,8 +1,10 @@
 // Let's fetch some data and filter out the results that
-// are not owned by "CFPB Administrator"
+// are not owned by "CFPB Administrator", then get a list
+// of owners' names
 fetch('http://data.consumerfinance.gov/api/views.json')
 .then(data => data.json())
 .then(data => {
     let owners = data.filter(doc => doc.owner.displayName !== 'CFPB Administrator')
-    console.log(owners)
+    let names = owners.map(doc => doc.owner.displayName)
+    console.log(names)
 })
